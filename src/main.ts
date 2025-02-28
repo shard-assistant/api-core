460fc61b9a8087f896f318b2903e646dba6a49a5
ClausCode <nclaus.code@gmail.com>
2025-02-28 23:06:38 +0300

    refactor: configure prettier

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,22 @@
-import { Logger } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
-import * as dotenv from "dotenv";
-import { AppModule } from "./app.module";
+import { Logger } from "@nestjs/common"
+import { NestFactory } from "@nestjs/core"
+import * as dotenv from "dotenv"
 
-export const LOGGER = new Logger("Auth Service");
+import { AppModule } from "./app.module"
+
+export const LOGGER = new Logger("Auth Service")
 
 async function bootstrap() {
-  dotenv.config();
+	dotenv.config()
 
-  const PORT = process.env.PORT ?? 3000;
+	const PORT = process.env.PORT ?? 3000
 
-  const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+	const app = await NestFactory.create(AppModule)
+	await app.listen(PORT)
 
-  LOGGER.log("------------------------------------");
-  LOGGER.log(`| 🚀 http://localhost:${PORT}/api`);
-  LOGGER.log(`| 🌐 Сервер запущен на порту: ${PORT}`);
-  LOGGER.log("------------------------------------");
+	LOGGER.log("------------------------------------")
+	LOGGER.log(`| 🚀 http://localhost:${PORT}/api`)
+	LOGGER.log(`| 🌐 Сервер запущен на порту: ${PORT}`)
+	LOGGER.log("------------------------------------")
 }
-bootstrap();
+bootstrap()
