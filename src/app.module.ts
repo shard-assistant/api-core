4595e2b4ab15d7c7d02f747cebaded2b2102ac8d
ClausCode <nclaus.code@gmail.com>
2025-02-28 23:08:11 +0300

    refactor: clear imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { KafkaModule } from "@/kafka/kafka.module";
+import { Module } from "@nestjs/common";
 
 @Module({ 
-  imports: [KafkaModule], 
-  controllers: [AppController],
-  providers: [AppService] 
+  imports: [KafkaModule]
 }) 
 export class AppModule {} 
