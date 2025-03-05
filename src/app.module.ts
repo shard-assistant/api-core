b2e397eec252e2e02ce189fdab0c0c04b40c0e52
ClausCode <nclaus.code@gmail.com>
2025-03-05 14:26:25 +0300

    feat: add docker compose services (postgres, redis)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
 
-import { KafkaModule } from "@/kafka/kafka.module"
-
 import { IS_DEV_ENV } from "./libs/common/utils/is-dev.util"
 
 @Module({
 	imports: [
-		KafkaModule,
 		ConfigModule.forRoot({
 			ignoreEnvFile: !IS_DEV_ENV,
 			isGlobal: true
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { Logger, ValidationPipe } from "@nestjs/common"
 import { ConfigService } from "@nestjs/config"
 import { NestFactory } from "@nestjs/core"
-import cookieParser from "cookie-parser"
+import { RedisStore } from "connect-redis"
+import * as cookieParser from "cookie-parser"
+import * as session from "express-session"
+import IORedis from "ioredis"
 
 import { AppModule } from "./app.module"
+import { StringValue, ms } from "./libs/common/utils/ms.util"
+import { parseBoolean } from "./libs/common/utils/parse-boolean.util"
 
 export const LOGGER = new Logger("Auth Service")
 
@@ -11,6 +16,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
 	const config = app.get(ConfigService)
+	const redis = new IORedis(config.getOrThrow<string>("REDIS_URI"))
 
 	app.use(cookieParser(config.getOrThrow<string>("COOKIES_SECRET")))
 
@@ -20,12 +26,32 @@ async function bootstrap() {
 		})
 	)
 
+	app.use(
+		session({
+			secret: config.getOrThrow<string>("SESSION_SECRET"),
+			name: config.getOrThrow<string>("SESSION_NAME"),
+			resave: true,
+			saveUninitialized: false,
+			cookie: {
+				domain: config.getOrThrow<string>("SESSION_DOMAIN"),
+				maxAge: ms(config.getOrThrow<StringValue>("SESSION_MAX_AGE")),
+				httpOnly: parseBoolean(config.getOrThrow<string>("SESSION_HTTP_ONLY")),
+				secure: parseBoolean(config.getOrThrow<string>("SESSION_SECURE")),
+				sameSite: "lax"
+			},
+			store: new RedisStore({
+				client: redis,
+				prefix: config.getOrThrow<string>("SESSION_FOLDER")
+			})
+		})
+	)
+
 	app.enableCors({
 		origin: config.getOrThrow<string>("ALLOWED_ORIGIN"),
 		credentials: true,
 		exposedHeaders: ["set-cookie"]
 	})
 
-	await app.listen(config.getOrThrow<number>("PORT"))
+	await app.listen(config.getOrThrow<number>("APPLICATION_PORT"))
 }
 bootstrap()
