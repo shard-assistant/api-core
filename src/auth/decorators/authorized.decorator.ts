0b9e42c7e3c2ee0ae8b717ee9ee5c73bb831260a
ClausCode <nclaus.code@gmail.com>
2025-03-10 11:31:01 +0300

    feat: add user profile endpoints with role-based authorization

--- src/auth/decorators/authorized.decorator.ts
+++ src/auth/decorators/authorized.decorator.ts
@@ -9,4 +9,3 @@ export const Authorized = createParamDecorator(
 		return data ? user && user[data] : user
 	}
 )
-2
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Controller, Get, HttpCode, HttpStatus, Param } from "@nestjs/common"
+import { UserRole } from "@prisma/__generated__"
 
-@Controller('user')
+import { Authorization } from "@/auth/decorators/auth.decorator"
+import { Authorized } from "@/auth/decorators/authorized.decorator"
+
+import { UserService } from "./user.service"
+
+@Controller("user")
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) {}
+
+	@Authorization()
+	@Get("profile/current")
+	@HttpCode(HttpStatus.OK)
+	public async findProfile(@Authorized("id") userId: string) {
+		return this.userService.findById(userId)
+	}
+
+	@Authorization(UserRole.ADMIN)
+	@Get("profile/:id")
+	@HttpCode(HttpStatus.OK)
+	public async findProfileById(@Param("id") userId: string) {
+		return this.userService.findById(userId)
+	}
 }
