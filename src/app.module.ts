53829c529a032f0f78b1982aac87552ae4dd7084
ClausCode <nclaus.code@gmail.com>
2025-03-12 16:29:21 +0300

    feat: add password recovery functionality with email template

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from "@nestjs/config"
 
 import { AuthModule } from "./auth/auth.module"
 import { EmailConfirmationModule } from "./auth/email-confirmation/email-confirmation.module"
+import { PasswordRecoveryModule } from "./auth/password-recovery/password-recovery.module"
 import { ProviderModule } from "./auth/provider/provider.module"
 import { IS_DEV_ENV } from "./libs/common/utils/is-dev.util"
 import { MailModule } from "./libs/mail/mail.module"
@@ -20,7 +21,8 @@ import { UserModule } from "./user/user.module"
 		UserModule,
 		ProviderModule,
 		MailModule,
-		EmailConfirmationModule
+		EmailConfirmationModule,
+		PasswordRecoveryModule
 	]
 })
 export class AppModule {}
